96579b004b7693a3b322a7dbbc363037e8055f09
Romain Potier Ferry <romainpotierferry@gmail.com>
2025-01-11 20:05:46 +0100

    remove useless comments

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pktmlt",
     version="0.1.0",
-    packages=find_packages(),  # Automatically finds all packages
-    install_requires=[],       # Add dependencies here if needed
+    packages=find_packages(),
+    install_requires=[],
 )
-
